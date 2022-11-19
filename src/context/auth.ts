9bc47e97266b250a1ab54a0292db03b86ea845e4
Bartosz Szczecinski <baphemot@gmail.com>
2022-11-19 21:08:46 +0100

    Add authorisation directive support

--- src/context/auth.ts
+++ src/context/auth.ts
@@ -1,5 +1,8 @@
+import { mapSchema, getDirective, MapperKind } from "@graphql-tools/utils";
 import { StandaloneServerContextFunctionArgument } from "@apollo/server/dist/esm/standalone";
+import { defaultFieldResolver, GraphQLSchema } from "graphql";
 import jwtDecode from "jwt-decode";
+import { Context } from ".";
 
 interface Token {
   id: string;
@@ -27,4 +30,69 @@ export const authContext = async (
   }
 
   return {};
+};
+
+export const authDirective = (
+  directiveName: string,
+  getUserFn: (token: string) => { isAuthorised: (role: string) => boolean }
+) => {
+  const typeDirectiveArgumentMaps: Record<string, any> = {};
+  return {
+    authDirectiveTypeDefs: `directive @${directiveName}(
+      requires: Role = USER,
+    ) on OBJECT | FIELD_DEFINITION
+ 
+    enum Role {
+      USER
+      UNKNOWN
+    }`,
+    authDirectiveTransformer: (schema: GraphQLSchema) =>
+      mapSchema(schema, {
+        [MapperKind.TYPE]: (type) => {
+          const authDirective = getDirective(schema, type, directiveName)?.[0];
+          if (authDirective) {
+            typeDirectiveArgumentMaps[type.name] = authDirective;
+          }
+          return undefined;
+        },
+        [MapperKind.OBJECT_FIELD]: (fieldConfig, _fieldName, typeName) => {
+          const authDirective =
+            getDirective(schema, fieldConfig, directiveName)?.[0] ??
+            typeDirectiveArgumentMaps[typeName];
+          if (authDirective) {
+            const { requires } = authDirective;
+            if (requires) {
+              const { resolve = defaultFieldResolver } = fieldConfig;
+              fieldConfig.resolve = function (
+                source,
+                args,
+                context: Context,
+                info
+              ) {
+                const user = getUserFn(context.auth.jwt);
+                if (!user.isAuthorised(requires)) {
+                  throw new Error("Not authorized");
+                }
+                return resolve(source, args, context, info);
+              };
+              return fieldConfig;
+            }
+          }
+        },
+      }),
+  };
+};
+
+export const getUser = (token: string) => {
+  return {
+    isAuthorised: (role: string) => {
+      switch (role) {
+        case "USER":
+          return !!token;
+        case "UNKNOWN":
+          return true;
+      }
+      return false;
+    },
+  };
 };
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,25 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { context } from "./context";
+import { makeExecutableSchema } from "@graphql-tools/schema";
+import { Context, context } from "./context";
+import { authDirective, getUser } from "./context/auth";
 
 import { resolvers } from "./resolvers";
 import typeDefs from "./typedefs";
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers: resolvers,
-});
+const { authDirectiveTypeDefs, authDirectiveTransformer } = authDirective(
+  "auth",
+  getUser
+);
+
+const schema = authDirectiveTransformer(
+  makeExecutableSchema({
+    typeDefs: [authDirectiveTypeDefs, typeDefs],
+    resolvers: resolvers,
+  })
+);
+
+const server = new ApolloServer<Context>({ schema });
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
