6cfd4ab8178e1bedd908405df2e652df339dc5d9
Bartosz Szczecinski <baphemot@gmail.com>
2022-11-19 22:24:20 +0100

    add missed file

---
+++ src/context/types.ts
@@ -0,0 +1,17 @@
+import { DataSources } from "../features";
+
+export interface Context {
+  auth: Auth;
+  dataSources: DataSources;
+}
+
+export interface Token {
+  id: string;
+  name: string;
+}
+
+export interface Auth {
+  id?: string;
+  name?: string;
+  jwt?: string;
+}
