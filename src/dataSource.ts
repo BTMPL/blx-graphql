a66aa562baf0f2b5fd4a9d5b4345e36d74d11dfd
Bartosz Szczecinski <baphemot@gmail.com>
2022-12-02 18:13:59 +0100

    Add uuid as x-request-id

--- src/dataSource.ts
+++ src/dataSource.ts
@@ -1,3 +1,4 @@
+import { v4 } from "uuid";
 import {
   RESTDataSource,
   WillSendRequestOptions,
@@ -26,6 +27,6 @@ export class AuthenticatedRESTDataSource extends RESTDataSource {
       request.headers["authorization"] = `Bearer ${this.token}`;
     }
 
-    request.headers["x-request-id"] = this.requestId;
+    request.headers["x-request-id"] = v4();
   }
 }
